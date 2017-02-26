d3858367e7372c3a68272d58c23b40364d7c441d
jz3p <jimdz@DESKTOP-VE8M0JL>
2017-02-26 22:00:37 +0800

    commit

---
+++ springcloudmain/src/main/java/spring/cotroller/SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package spring.cotroller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringCloudApplication {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(SpringCloudApplication.class).web(true).run(args);
+	}
+}
